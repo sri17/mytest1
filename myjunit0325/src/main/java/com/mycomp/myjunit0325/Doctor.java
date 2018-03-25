f474feaf61609158ab25d196095dd0ade0e14452
sri18 <sri9186@yahoo.com>
2018-03-25 17:04:13 -0500

    firsttime

---
+++ myjunit0325/src/main/java/com/mycomp/myjunit0325/Doctor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycomp.myjunit0325;
+
+/**
+ *
+ * @author T420
+ */
+public class Doctor {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
